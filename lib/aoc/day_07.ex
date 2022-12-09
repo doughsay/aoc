e6ea9b9c6f4995a5d3c46dbab1dff2500e27ca5d
Chris Dosé <221693+doughsay@users.noreply.github.com>
2022-12-08 18:34:54 -0800

    Final day 7

--- lib/aoc/day_07.ex
+++ lib/aoc/day_07.ex
@@ -1,151 +1,6 @@
 defmodule AoC.Day07 do
   @moduledoc File.read!("./lib/aoc/day_07.md")
 
-  def part_one do
-    data()
-    |> build_tree()
-
-    # |> calculate_dir_sizes()
-    # |> traverse_tree()
-  end
-
-  # defp calculate_dir_sizes(tree) do
-  #   # {files, _dirs} = Map.pop(tree, :files)
-  #   # files |> Enum.map(&elem(&1, 0)) |> Enum.sum()
-  # end
-
-  # defp traverse_tree(tree) when map_size(tree) == 1 do
-  #   IO.inspect(tree, label: "leaf")
-  #   Map.put(tree, :size, get_size(tree[:files]))
-  # end
-
-  defp traverse_tree(tree) do
-    # tree = Map.put(tree, :size, get_size(tree[:files]))
-
-    # Map.new(tree, fn
-    #   {:files, files} ->
-    #     {:files, files}
-
-    #   {:size, size} ->
-    #     {:size, size}
-
-    #   {dir, child_tree} ->
-    #     # IO.inspect(dir, label: "node")
-    #     {dir, traverse_tree(child_tree)}
-    # end)
-
-    ###
-
-    my_size = tree[:size]
-
-    sizes =
-      Enum.map(tree, fn
-        {dir, child_tree} when is_binary(dir) ->
-          traverse_tree(child_tree)
-
-        _other ->
-          0
-      end)
-
-    # |> IO.inspect()
-
-    # {child_sizes, new_children} = Enum.unzip(new_children_and_their_sizes)
-    # IO.inspect(new_children)
-    # new_tree = Map.new(new_children)
-
-    my_total_size = Enum.sum(sizes) + my_size
-
-    if my_total_size > 3_636_666, do: IO.inspect(my_total_size)
-    # new_me = Map.put(new_tree, :size, my_total_size)
-
-    my_total_size
-  end
-
-  def tree_size(tree) do
-    (tree
-     |> Enum.map(fn
-       {dir, child_tree} when is_binary(dir) ->
-         tree_size(child_tree)
-
-       _other ->
-         0
-     end)
-     |> Enum.sum()) + tree[:size]
-  end
-
-  def depth_first(tree) do
-    my_size = tree[:size]
-
-    x =
-      Enum.flat_map(tree, fn
-        {:size, _value} -> []
-        {subdir, subtree} -> [depth_first(subtree)]
-      end)
-  end
-
-  def zipper do
-    ExZipper.Zipper.zipper(
-      fn node -> is_map(node) and Map.has_key?(node, :dirs) end,
-      fn %{dirs: dirs} -> dirs end,
-      fn node, new_dirs -> %{node | dirs: new_dirs} end,
-      %{name: "/", dirs: [], size: 0}
-    )
-  end
-
-  # defp get_size(files), do: files |> Enum.map(&elem(&1, 0)) |> Enum.sum()
-
-  def part_two do
-    data()
-  end
-
-  defp data do
-    "./data/day_07.txt"
-    |> File.read!()
-    |> String.trim()
-    |> String.split("\n")
-  end
-
-  defp build_tree(lines) do
-    {_final_pwd, tree} = Enum.reduce(lines, {["/"], %{"/" => %{size: 0}}}, &process_line/2)
-    tree["/"]
-  end
-
-  defp process_line("$ " <> command, state), do: process_command(command, state)
-
-  defp process_line("dir " <> dirname, {pwd, tree}) do
-    {pwd, add_dir(tree, pwd, dirname)}
-  end
-
-  defp process_line(size_and_filename, {pwd, tree}) do
-    size = parse_file_size(size_and_filename)
-    {pwd, add_file(tree, pwd, size)}
-  end
-
-  defp process_command("cd /", {_pwd, tree}), do: {["/"], tree}
-  defp process_command("cd ..", {[_ | parent], tree}), do: {parent, tree}
-  defp process_command("cd " <> path, {pwd, tree}), do: {[path | pwd], tree}
-  defp process_command("ls", state), do: state
-
-  defp parse_file_size(size_and_filename) do
-    [size, _filename] = String.split(size_and_filename, " ")
-    String.to_integer(size)
-  end
-
-  defp add_dir(tree, pwd, dirname) do
-    update_in(tree, Enum.reverse([dirname | pwd]), fn
-      nil -> %{size: 0}
-      already_exists -> already_exists
-    end)
-  end
-
-  defp add_file(tree, pwd, size) do
-    update_in(tree, Enum.reverse([:size | pwd]), &(&1 + size))
-  end
-end
-
-defmodule AoC.Day07Zipper do
-  @moduledoc File.read!("./lib/aoc/day_07.md")
-
   alias ExZipper.Zipper
 
   def part_one do
@@ -155,13 +10,31 @@ defmodule AoC.Day07Zipper do
     |> Enum.sum()
   end
 
+  @total_disk_size 70_000_000
+  @unused_required 30_000_000
+
+  def part_two do
+    tree = data()
+
+    total_used = size_of_tree(tree)
+    total_unused = @total_disk_size - total_used
+    delete_at_least = @unused_required - total_unused
+
+    tree
+    |> Stream.unfold(&get_all_sizes/1)
+    |> Enum.sort()
+    |> Enum.find(&(&1 >= delete_at_least))
+  end
+
   defp get_all_sizes(tree) do
     if Zipper.end?(tree) do
       nil
     else
-      {size_of_tree(tree.focus), Zipper.next(tree)}
+      {size_of_tree(tree), Zipper.next(tree)}
     end
   end
+
+  defp size_of_tree(%Zipper{focus: node}), do: size_of_tree(node)
 
   defp size_of_tree(node) do
     node.size +
--- test/aoc/day_07_test.exs
+++ test/aoc/day_07_test.exs
@@ -4,10 +4,10 @@ defmodule AoC.Day07Test do
   alias AoC.Day07
 
   test "gets the right answer for part one" do
-    assert Day07.part_one() == 0
+    assert Day07.part_one() == 1_297_159
   end
 
   test "gets the right answer for part two" do
-    assert Day07.part_two() == 0
+    assert Day07.part_two() == 3_866_390
   end
 end
