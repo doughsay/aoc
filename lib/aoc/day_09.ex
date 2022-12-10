cefd3f1b29a00bc8b77108f2ae18451c9e22126b
Chris Dosé <221693+doughsay@users.noreply.github.com>
2022-12-09 20:22:45 -0800

    Simplify Day 9

--- lib/aoc/day_09.ex
+++ lib/aoc/day_09.ex
@@ -1,10 +1,10 @@
 defmodule AoC.Day09 do
   @moduledoc File.read!("./lib/aoc/day_09.md")
 
-  defstruct head: {0, 0}, tail: [], visited: MapSet.new()
+  defstruct knots: [], visited: MapSet.new()
 
-  defp new(tail_count \\ 1),
-    do: %__MODULE__{tail: Enum.map(0..(tail_count - 1), fn _ -> {0, 0} end)}
+  def new(knot_count \\ 2),
+    do: %__MODULE__{knots: Enum.map(0..(knot_count - 1), fn _ -> {0, 0} end)}
 
   def part_one do
     data()
@@ -13,63 +13,34 @@ defmodule AoC.Day09 do
     |> Enum.count()
   end
 
-  defp move({:up, count}, state), do: do_move(state, {0, 1}, count)
-  defp move({:down, count}, state), do: do_move(state, {0, -1}, count)
-  defp move({:right, count}, state), do: do_move(state, {1, 0}, count)
-  defp move({:left, count}, state), do: do_move(state, {-1, 0}, count)
-
-  defp do_move(state, dir, count) do
-    Enum.reduce(1..count, state, fn _, state ->
-      new_head = add(state.head, dir)
-
-      {_, new_tail} =
-        Enum.reduce(state.tail, {new_head, []}, fn tail, {head, acc} ->
-          new_tail = fix_tail(head, tail)
-          {new_tail, [new_tail | acc]}
+  def move({dir, count}, rope) do
+    Enum.reduce(1..count, rope, fn _i, %{knots: [head | tail]} = rope ->
+      new_knots =
+        Enum.scan([add(head, dir) | tail], fn tail, head ->
+          fix_tail(head, tail)
         end)
 
-      new_visited = MapSet.put(state.visited, hd(new_tail))
+      new_visited = MapSet.put(rope.visited, List.last(new_knots))
 
-      %{state | head: new_head, tail: Enum.reverse(new_tail), visited: new_visited}
+      %{rope | knots: new_knots, visited: new_visited}
     end)
   end
 
   defp add({x, y}, {u, v}), do: {x + u, y + v}
   defp sub({x, y}, {u, v}), do: {x - u, y - v}
 
-  defp fix_tail(head, tail), do: sub(tail, head) |> do_fix_tail(tail)
+  defp fix_tail(head, tail), do: tail |> sub(head) |> fix() |> add(tail)
 
-  # up
-  defp do_fix_tail({0, -2}, tail), do: add(tail, {0, 1})
-  defp do_fix_tail({-1, -2}, tail), do: add(tail, {1, 1})
-  defp do_fix_tail({1, -2}, tail), do: add(tail, {-1, 1})
+  defp fix({x, y}) when abs(x) <= 1 and abs(y) <= 1, do: {0, 0}
+  defp fix({x, y}), do: {opposite_sign(x), opposite_sign(y)}
 
-  # down
-  defp do_fix_tail({0, 2}, tail), do: add(tail, {0, -1})
-  defp do_fix_tail({-1, 2}, tail), do: add(tail, {1, -1})
-  defp do_fix_tail({1, 2}, tail), do: add(tail, {-1, -1})
-
-  # right
-  defp do_fix_tail({-2, 0}, tail), do: add(tail, {1, 0})
-  defp do_fix_tail({-2, -1}, tail), do: add(tail, {1, 1})
-  defp do_fix_tail({-2, 1}, tail), do: add(tail, {1, -1})
-
-  # left
-  defp do_fix_tail({2, 0}, tail), do: add(tail, {-1, 0})
-  defp do_fix_tail({2, -1}, tail), do: add(tail, {-1, 1})
-  defp do_fix_tail({2, 1}, tail), do: add(tail, {-1, -1})
-
-  # diagonals
-  defp do_fix_tail({2, 2}, tail), do: add(tail, {-1, -1})
-  defp do_fix_tail({-2, -2}, tail), do: add(tail, {1, 1})
-  defp do_fix_tail({-2, 2}, tail), do: add(tail, {1, -1})
-  defp do_fix_tail({2, -2}, tail), do: add(tail, {-1, 1})
-
-  defp do_fix_tail({x, y}, tail) when abs(x) <= 1 and abs(y) <= 1, do: tail
+  defp opposite_sign(0), do: 0
+  defp opposite_sign(x) when x > 0, do: -1
+  defp opposite_sign(x) when x < 0, do: 1
 
   def part_two do
     data()
-    |> Enum.reduce(new(9), &move/2)
+    |> Enum.reduce(new(10), &move/2)
     |> Map.get(:visited)
     |> Enum.count()
   end
@@ -85,8 +56,8 @@ defmodule AoC.Day09 do
   defp decode_move(<<dir::binary-size(1)>> <> " " <> count),
     do: {direction(dir), String.to_integer(count)}
 
-  defp direction("U"), do: :up
-  defp direction("D"), do: :down
-  defp direction("L"), do: :left
-  defp direction("R"), do: :right
+  defp direction("U"), do: {0, 1}
+  defp direction("D"), do: {0, -1}
+  defp direction("L"), do: {-1, 0}
+  defp direction("R"), do: {1, 0}
 end
